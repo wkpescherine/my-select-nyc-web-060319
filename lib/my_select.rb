d2578d8354c28d11f85490b30731e218f172b4d6
William Pescherine <wkpescherine@gmail.com>
2019-05-27 01:52:33 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,16 @@
 def my_select(collection)
  # your code here!
+  def my_select(array)
+  i = 0
+  select = []
+  while i < array.length
+    if yield(array[i])
+      select << array[i]
+    end
+    i+=1
+  end
+  select
+end
+
+my_select(collection) { | num | num.even? }
 end
